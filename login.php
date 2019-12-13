5e443bb2b6d1412a223895d727b0291e541c0628
artemais <artemaisproject@gmail.com>
2019-12-13 11:47:07 -0300

    Header em contrução

--- login.php
+++ login.php
@@ -3,12 +3,63 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
+		<meta charset="UTF-8">
+  	<meta name="description" content="Revista Eletrônica">
+  	<meta name="keywords" content="Arte, Tecnologia, Games">
+  	<meta name="author" content="Arte +">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="assets/main.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 		<title>Login</title>
+
 	</head>
 	<body>
+
+		<!-- HEADER -->
+		<header class="bg-artemais">
+			<div class="row text-center">
+				<div class="col"></div>
+				<div class="col">
+					<a class="" href="index.php">
+						<img src="assets/img/logoartemais(350x350).png" width="130px" height="100px">
+					</a>
+				</div>
+				<div class="col"></div>
+			</div>
+			<nav class="navbar navbar-expand-md bg-artemais">
+				<!-- Navigation -->
+				<li class="nav-item">
+					<a href="index.php"><i class="fas fa-home"></i></a>
+				</li>
+				<li class="nav-item">
+					<a class="category" href="#">Tecnologia</a>
+				</li>
+				<li class="nav-item">
+					<a class="category" href="#">Ciência</a>
+				</li>
+				<li class="nav-item">
+					<a class="category" href="#">Música</a>
+				</li>
+				<li class="nav-item">
+					<a class="category" href="#">Cinema</a>
+				</li>
+				<li class="nav-item">
+					<a class="category" href="#">Games</a>
+				</li>
+
+			</nav>
+		</header>
+
+		<!-- END HEADER -->
+
+		<!-- LOGIN -->
 		<h4>Login</h4>
 
 		<form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
@@ -52,6 +103,9 @@
 
 			}
 		?>
+
+		<!-- END LOGIN -->
+
 	</body>
 </html>
 
