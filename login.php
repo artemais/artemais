47d36df869248d487ac78711777c4416388856b5
artemais <artemaisproject@gmail.com>
2020-01-10 08:56:18 -0300

    Cadastro e Login adicionados, sem layout oficial

--- login.php
+++ login.php
@@ -75,14 +75,63 @@
 		<!-- END HEADER -->
 
 		<!-- LOGIN -->
-		<h4>Login</h4>
 
-		<form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
-			<input type="email" name="email" placeholder="Email" required><br><br>
-			<input type="password" name="password" placeholder="Senha" required><br><br>
-			<input type="submit"  name="signin" value="Login">
-			<p>Não tem conta? <a href="cadastro.php">Cadastre-se</a>
-		</form>
+    <div class="container-fluid">
+    	<div class="row justify-content-center align-self-center">
+    		<div class="col col-sm-6 col-md-6 col-lg-4 col-xl-3 h-100">
+					<form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+						<h4>Login</h4>
+						<input type="email" name="email" placeholder="Email" class="form-control" required><br><br>
+						<input type="password" name="password" placeholder="Senha" class="form-control" required><br><br>
+						<input type="submit"  name="signin" value="Login">
+
+						<!-- CADASTRO -->
+						<p>Não tem conta?</p>
+							<!-- Button to Open the Modal -->
+								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+  								Cadastro
+								</button>
+							<!-- The Modal -->
+							<div class="modal fade" id="myModal">
+  							<div class="modal-dialog">
+    							<div class="modal-content">
+
+      							<!-- Modal Header -->
+      							<div class="modal-header">
+        							<h4 class="modal-title">Cadastro</h4>
+        							<button type="button" class="close" data-dismiss="modal">&times;</button>
+      							</div>
+
+      							<!-- Modal body -->
+      							<div class="modal-body">
+        							<form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+        								<input type="text" name="first_name" placeholder="Nome" required><br><br>
+        								<input type="text" name="last_name" placeholder="Sobrenome" required><br><br>
+        								<input type="text" name="nick" placeholder="Apelido" required><br><br>
+        								<input type="email" name="email" placeholder="Email" required><br><br>
+        								<input type="password" name="password" placeholder="Senha" required><br><br>
+        								<input type="password" name="password2" placeholder="Confirmar" required><br><br>
+        								<input type="checkbox" name="privacy" required><small>Eu li e concordo com os</small>
+          							<a href="privacy.php" target="_blank">
+            							<small>Termos de Uso.</small><br><br>
+          							</a>
+        								<input type="submit" name="signup" value="Enviar">
+      								</form>
+
+      							</div>
+
+      							<!-- Modal footer -->
+      							<div class="modal-footer">
+        							<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      							</div>
+
+    							</div>
+  							</div>
+							</div>
+					</form>
+				</div>
+			</div>
+		</div>
 
 		<?php
 			include("db_connect.php");
@@ -120,6 +169,75 @@
 		?>
 
 		<!-- END LOGIN -->
+
+
+		<!-- CADASTRO -->
+		<?php
+        include("db_connect.php");
+ 
+        if ( isset($_POST['signup']) )
+        {
+          $first_name = $_POST['first_name'];
+          $last_name = $_POST['last_name'];
+          $nick = $_POST['nick'];
+          $email = $_POST['email'];
+          $password = $_POST['password'];
+          $password2 = $_POST['password2'];
+ 
+          if ( $first_name == '' )
+          {
+           echo "<p>Por favor, insira um nome!</p>";
+            exit();
+          }
+          if ( $last_name == '' )
+          {
+            echo "<p>Por favor, insira um sobrenome!</p>";
+            exit();
+          }
+          if ( $nick == '' )
+          {
+            echo "<p>Por favor, insira um nick!</p>";
+            exit();
+          }
+          if ( $email == '' )
+          {
+            echo "<p>Por favor, insira um email!</p>";
+            exit();
+          }
+          if ( $password == '' )
+         {
+            echo "<p>Por favor, insira uma senha!</p>";
+            exit();
+          }
+          if ( $password2 == '' )
+          {
+            echo "<p>Por favor, confirme a senha!</p>";
+            exit();
+          }
+          if ( $password != $password2 )
+          {
+            echo "<p>As senhas devem ser iguais!</p>";
+            exit();
+          }
+$verify_email_query = "SELECT * FROM user WHERE user_email = '$email'";
+          $verify_email = mysqli_query($dbcon, $verify_email_query);
+ 
+          if ( mysqli_num_rows($verify_email) > 0)
+          {
+            echo "<p>O email $email já existe!</p>";
+            exit();
+          }
+
+          $insert_user = "INSERT INTO user (first_name, last_name, nick, user_email, user_password) VALUES     ('$first_name', '$last_name', '$nick', '$email', '$password')";
+ 
+          if ( mysqli_query($dbcon, $insert_user) )
+          {
+            echo "<p>Obrigado por cadastrar-se, faça <a href='login.php'>login</a>!</p>";
+         }
+        }
+      ?>
+
+		<!-- END CADASTRO -->
 
 	</body>
 </html>
